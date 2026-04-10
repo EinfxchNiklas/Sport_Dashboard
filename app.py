769a209508643a0499c5229c42a98bfdd708b557
EinfxchNiklas <ihmelsn@gmail.com>
2026-04-11 01:05:26 +0200

    auto reload bei speichern einer datei eingebaut

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def american_football_placeholder():
 if __name__ == '__main__':
     import os
     debug_mode = os.environ.get('DEBUG', 'False') == 'True'
-    app.run(debug=debug_mode, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), use_reloader=True)
